fb12ad6c5769196b75fc1af3b5432352f6ff4284
Justin Lawson <jaglawson3@gmail.com>
2016-02-23 11:31:12 -0700

    The struggle is the education

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,12 @@
 angular.module('gwarRoom', ['ui.router'])
-
-  .config(function($stateRouterProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
     console.log("GwarRoom app.js is alive!")
+
     $urlRouterProvider.otherwise('/');
 
     $stateProvider.state('/war-room', {
       templateUrl: 'war-room.html',
       controller: 'MainController',
       url: '/war-room'
-    }).state('detail', {
-      templateUrl: 'templates/details.html',
-      controller: 'DetailController',
-      url: '/home/:id'
     })
   });
--- client/app/controllers.js
+++ client/app/controllers.js
@@ -1,15 +1,8 @@
 angular.module('gwarRoom')
-  .controller('MainController', MainController)
-  .controller('DetailController', DetailController);
+  .controller('MainController', MainController);
 
-HomeController.$inject = ['$scope', 'GWarService'];
+MainController.$inject = ['$scope', 'GWarService'];
 
 function MainController($scope, GWarService) {
   console.log("Hello from Home Controller")
-}
-
-DetailController.$inject = ['$scope', 'GWarService']
-
-function DetailController($scope, GWarService) {
-  console.log("Hello from the Details Controller")
 }
